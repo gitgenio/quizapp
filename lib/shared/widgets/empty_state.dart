6ce9456b4326ba7ad25c89ffc9b8f98b07767f87
GitGenio <catorog@unal.edu.co>
2026-07-10 23:01:16 -0500

    empty_state

---
+++ lib/shared/widgets/empty_state.dart
@@ -0,0 +1,36 @@
+import 'package:flutter/material.dart';
+
+class EmptyState extends StatelessWidget {
+  final String message;
+  final IconData icon;
+
+  const EmptyState({
+    super.key,
+    required this.message,
+    this.icon = Icons.inbox_outlined,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Column(
+        mainAxisSize: MainAxisSize.min,
+        children: [
+
+          Icon(
+            icon,
+            size: 70,
+          ),
+
+          const SizedBox(height: 16),
+
+          Text(
+            message,
+            style: Theme.of(context).textTheme.bodyLarge,
+            textAlign: TextAlign.center,
+          ),
+        ],
+      ),
+    );
+  }
+}
