f4cf24a33d98f9a94278382aad03a612fd889ff6
GitGenio <catorog@unal.edu.co>
2026-07-12 17:36:59 -0500

    PageHeader

---
+++ lib/features/admin/widgets/page_header.dart
@@ -0,0 +1,54 @@
+import 'package:flutter/material.dart';
+
+class PageHeader extends StatelessWidget {
+
+  final String title;
+  final String subtitle;
+  final Widget? action;
+
+  const PageHeader({
+    super.key,
+    required this.title,
+    required this.subtitle,
+    this.action,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+
+    return Row(
+
+      crossAxisAlignment: CrossAxisAlignment.start,
+
+      children: [
+
+        Expanded(
+
+          child: Column(
+
+            crossAxisAlignment: CrossAxisAlignment.start,
+
+            children: [
+
+              Text(
+                title,
+                style: Theme.of(context).textTheme.headlineMedium,
+              ),
+
+              const SizedBox(height:8),
+
+              Text(
+                subtitle,
+                style: Theme.of(context).textTheme.bodyLarge,
+              )
+
+            ],
+          ),
+        ),
+
+        if(action!=null) action!
+
+      ],
+    );
+  }
+}
