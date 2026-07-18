83ece02b5b0a653320fc060b482e3ea067910fb7
GitGenio <catorog@unal.edu.co>
2026-07-17 22:45:16 -0500

    SupabaseConfig

---
+++ lib/core/constants/supabase_config.dart
@@ -0,0 +1,8 @@
+class SupabaseConfig {
+  const SupabaseConfig._();
+
+  static const String url =  'https://lrbodcnyhdjfgpinfyqf.supabase.co';;
+
+  static const String anonKey =  'sb_publishable_Rthi9dsKJYH-qOISQdnr-g_AJGTCfcX';
+}
+
