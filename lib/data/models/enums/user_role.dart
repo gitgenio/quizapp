cadf931c659e1b9d21b9f56e95cce4572e4bf4fd
GitGenio <catorog@unal.edu.co>
2026-07-18 00:54:36 -0500

    modify user_role

--- lib/data/models/enums/user_role.dart
+++ lib/data/models/enums/user_role.dart
@@ -1,5 +1,8 @@
-/// Define el rol del usuario dentro de la aplicación.
+/// Roles disponibles dentro de QuizApp.
+///
+/// Los participantes no tienen cuenta en el sistema,
+/// por lo que únicamente existen roles administrativos.
 enum UserRole {
+  superAdmin,
   administrator,
-  participant,
 }
